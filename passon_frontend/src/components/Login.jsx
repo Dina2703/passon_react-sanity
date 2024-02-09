4e89f3d6862d4c64b2c42afe757ef513c7060cf9
Dinara Idrissova <Dinara.Idrissova@list.ru>
2024-02-09 00:09:32 -0500

    fetching user data on successful login

--- passon_frontend/src/components/Login.jsx
+++ passon_frontend/src/components/Login.jsx
@@ -1,15 +1,45 @@
-import React from "react";
-import GoogleLogin from "react-google-login";
+import { useState, useEffect } from "react";
+import { useGoogleLogin } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import video from "../assets/video.mp4";
 import whiteLogo from "../assets/whiteLogo.png";
-
-function responseGoogle(response) {
-  console.log(response);
-}
+import axios from "axios";
 
 const Login = () => {
+  const [user, setUser] = useState([]);
+  const [profile, setProfile] = useState({});
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => {
+      setUser(codeResponse);
+      console.log(codeResponse);
+      localStorage.setItem("user", JSON.stringify(codeResponse));
+    },
+
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+          console.log(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
   return (
     <div className="flex justify-start flex-col h-screen items-center">
       <div className="relative w-full h-full">
@@ -27,22 +57,13 @@ const Login = () => {
             <img src={whiteLogo} alt="logo" width="130px" />
           </div>
           <div className="shadow-2xl">
-            <GoogleLogin
-              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
-              render={(renderProps) => (
-                <button
-                  type="button"
-                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
-                  onClick={renderProps.onClick}
-                  disabled={renderProps.disabled}
-                >
-                  <FcGoogle className="mr-4" /> Sign in with Google
-                </button>
-              )}
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
-              cookiePolicy="single_host_origin"
-            />
+            <button
+              type="button"
+              className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+              onClick={() => login()}
+            >
+              <FcGoogle className="mr-4" /> Sign in with Google
+            </button>
           </div>
         </div>
       </div>
--- passon_frontend/src/index.js
+++ passon_frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
     <App />
-  </React.StrictMode>
+  </GoogleOAuthProvider>
 );
