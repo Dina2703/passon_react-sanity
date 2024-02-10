dee4a7c241f752a1b9f18a37a9021d76264dc84c
Dinara Idrissova <Dinara.Idrissova@list.ru>
2024-02-09 22:31:01 -0500

    Pins: create related pages, and setup router for them

--- passon_frontend/src/components/Pins.jsx
+++ passon_frontend/src/components/Pins.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Feed from "./Feed";
+import PinDetails from "./PinDetails";
+import Search from "./Search";
+import CreatePin from "./CreatePin";
 
-const Pins = () => {
-  return <div>Pins</div>;
+const Pins = ({ user }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <div className="px-2 md:px-5">
+      <div className="bg-gray-50">
+        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route path="/pin-details" element={<PinDetails user={user} />} />
+          <Route path="/create-pin" element={<CreatePin user={user} />} />
+          <Route
+            path="/search"
+            element={
+              <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            }
+          />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default Pins;
