1db2f9638f4e91e3f03345fed93c5799aa12d358
Dinara Idrissova <Dinara.Idrissova@list.ru>
2024-02-12 13:26:41 -0500

    Spinner component with  the 'react-loader-spinner'

---
+++ passon_frontend/src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Rings } from "react-loader-spinner";
+
+const Spinner = ({ message }) => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <Rings color="#00BFFF" height={50} width={200} />
+      <p className="text- lg text-center px-2">{message}</p>
+    </div>
+  );
+};
+
+export default Spinner;
